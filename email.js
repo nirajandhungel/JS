201e9447668d685a55585b128112c64e089567cf
nirajandhungel <subashdhungel555@gmail.com>
2025-08-14 23:21:10 +0545

    In Progress email validate

--- email.js
+++ email.js
@@ -1,6 +1,14 @@
 function validateEmail(email){
     let index =  email.indexOf('@');
     if(!index) return false;
-    return index;
+    // return index;
+    if(!email.endsWith(".com")) return false;
+    let count; //count of letters before @
+
+    for(let char of email){
+        if(char>='a' && char<='z' || char >='A' && char<='Z'){
+            count++;
+        }
+    }
 }
 console.log(validateEmail("subash@gmail.com"))
