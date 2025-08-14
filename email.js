1bea7437c59f6d29ad456dbbb6a0694ba213957d
nirajandhungel <subashdhungel555@gmail.com>
2025-08-14 23:29:18 +0545

    Complete email validation

--- email.js
+++ email.js
@@ -3,12 +3,22 @@ function validateEmail(email){
     if(!index) return false;
     // return index;
     if(!email.endsWith(".com")) return false;
-    let count; //count of letters before @
+    let localPart = email.slice(0,index);
+    let count=0; //count of letters before @
+    let haveNumber = false;
 
-    for(let char of email){
-        if(char>='a' && char<='z' || char >='A' && char<='Z'){
+    for(let char of localPart){
+        if((char>='a' && char<='z') || (char >='A' && char<='Z')){
             count++;
         }
+
+        if(char>=0 && char<=9){
+            haveNumber = true;
+        }
     }
+    if(count<6) return false;
+    if(!haveNumber) return false;
+
+    return true;
 }
-console.log(validateEmail("subash@gmail.com"))
+console.log(validateEmail("sub32@gmail.com"))
